11298e8a117ffd5ee6544cac03e1fb2f4e6dbbad
Daniel Moniz <daniel.moniz@gmail.com>
2012-04-12 15:04:05 -0400

    Improve script output. Make executable.
    Also add comments to improve readability.
    Add a TODO relating to numbered bullets.

--- tumblr_format.py
+++ tumblr_format.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python
 import sys
 
 # Get file from argument
-filename = sys.argv[1]
+try:
+    filename = sys.argv[1]
+except IndexError:
+    print "Need a target file! Enter the name of the file as a parameter."
+    sys.exit(0)
 
-from_file = open(filename, 'r')
-to_file = open(filename + ".tumblr", 'w')
+try:
+    from_file = open(filename, 'r')
+except IOError:
+    print "File does not exist!"
+    sys.exit(0)
+
+output_filename = filename + ".tumblr"
+to_file = open(output_filename, 'w')
 
 def remove_newline(line):
     return line[:-1]
@@ -12,11 +23,14 @@ def remove_newline(line):
 def starts_with_numbered_bullet(line):
     """Returns True if the line begins with a numbered bullet, False
     otherwise."""
+    # Strip the start of the line for whitespace in case of bullets like, " - "
     line = line.strip()
     if line[:1].isdigit():
         for char in line[1:]:
             # Keep searching until a non-digit is found. If not a digit, only
             # the '.' character is acceptable. Otherwise, return False.
+            # Eg. "34." is okay, but "34b." is not.
+            # @TODO Possibly allow single letter after the integer.
             if char.isdigit():
                 continue
             else:
@@ -27,10 +41,10 @@ def starts_with_numbered_bullet(line):
 
     return False
 
-
 def starts_with_bullet(line):
     """Return True if a string starts with a bullet, and False otherwise."""
     list_of_bullets = ['*', '-', '+']
+    # Strip the start of the line for whitespace in case of bullets like, " - "
     line = line.strip()
     for bullet in list_of_bullets:
         if line.startswith(bullet):
@@ -43,14 +57,17 @@ def starts_with_bullet(line):
 
 # Remove the first two lines: these lines are generally titles
 lines = from_file.readlines()
-del lines[:2]
+try:
+    if lines[1] == '\n':
+        del lines[:2]
+except IndexError:
+    # Simply do not delete any lines.
+    pass
 
+# Keep track of a list of lines to be concatenated and written to the output
+# file.
 current_line_list = []
-line_num = 2
 for line in lines:
-    line_num += 1
-    # Used for debugging purposes
-    #print line_num, line
     # If a newline after we've stored content, print everything so far into one
     # line.
     if line == "\n" and len(current_line_list) > 0:
@@ -68,5 +85,8 @@ for line in lines:
 text = " ".join(current_line_list)
 to_file.write(text)
 
+# Close files and output a final message.
 from_file.close()
 to_file.close()
+print "Output tumblr-formatted file to {}.".format(output_filename)
+
